1e208ebbf3347fb72859a12cc1fd4c01eedc6105
Anderson Dario <anderson@jp7.com.br>
2017-02-02 11:52:10 -0200

    Fix port environment

--- server/database/connect.js
+++ server/database/connect.js
@@ -7,7 +7,7 @@ module.exports.connect = (app) => {
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
       // Initialize the app.
-      var server = app.listen(process.env.REACT_APP_MONGO_PORT || 8080, function () {
+      var server = app.listen(process.env.PORT || 8080, function () {
         var port = server.address().port;
         console.log("App now running on port", port);
       });
