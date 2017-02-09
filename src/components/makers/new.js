a0f8911d8c481d9b5e72fb38d03986166189edd9
JP Barbosa <jp@jp7.com.br>
2017-02-09 10:30:22 -0200

    Fix new maker redirect

--- src/components/makers/new.js
+++ src/components/makers/new.js
@@ -1,22 +1,25 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import Redirect from 'react-router/Redirect';
 import { createMaker } from '../../actions/index';
 import MakersForm from './form';
 
 class New extends Component {
-  static contextTypes = {
-    router: PropTypes.object
-  };
+  state = { saved: false };
 
   onSubmit(props) {
     this.props.createMaker(props)
-      .then(() => {
-        this.context.router.push('/makers');
+      .then((response) => {
+        this.setState({ saved: true });
       });
   }
 
   render() {
+    if (this.state.saved) {
+      return <Redirect to="/makers" />;
+    }
+
     return (
       <div className='ui container form-new'>
         <h3>Create a New Maker</h3>
