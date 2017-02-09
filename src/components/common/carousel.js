87b95f6a81d5efe1f049ec426d2a8a685cc9683a
Anderson Dario <anderson@jp7.com.br>
2017-02-09 12:41:00 -0200

    Fix broken carousel

--- src/components/common/carousel.js
+++ src/components/common/carousel.js
@@ -13,14 +13,12 @@ const Carousel = ({ items }) => {
   if (items.length) {
     const settings = {
       dots: false,
-      infinite: false,
+      infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
       utoplay: true,
       autoplaySpeed: 3000,
-      centerMode: true,
-      centerPadding: '60px',
       nextArrow: <SampleArrow classeName='slicker slicker-next' position='right' />,
       prevArrow: <SampleArrow classeName='slicker slicker-prev' position='left' />
     };
